2db7fb23e905b13e91c364a264643ee1fd249aab
Leo Cheung <lappangcheung@gmail.com>
2019-01-02 21:39:17 -0500

    fix issue with typo

--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -44,5 +44,5 @@ class TestProductionConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['TESTING'])
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     unittest.main()
