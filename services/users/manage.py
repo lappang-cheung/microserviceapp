16016bc3e346059984cf9c91eff2ea58def8119b
Leo Cheung <lappangcheung@gmail.com>
2019-01-06 21:07:23 -0500

    update the flask blueprint

--- services/users/manage.py
+++ services/users/manage.py
@@ -4,9 +4,14 @@ import unittest
 
 from flask.cli import FlaskGroup
 
-from project import app
+from project import create_app, db # new
+from project.api.models import User # new
 
-cli = FlaskGroup(app)
+app = create_app() # new
+cli = FlaskGroup(create_app=create_app) # new
+
+app = create_app() # new
+cli = FlaskGroup(create_app=create_app) # new
 
 # new
 @cli.command()
